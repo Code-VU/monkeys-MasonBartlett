962349f01e6626e6e7f0082b9d09a48faa100853
Mason Bartlett <Mason.Bartlett@vu.com>
2022-02-14 21:30:59 -0600

    first

--- monkeyCalculator.py
+++ monkeyCalculator.py
@@ -3,6 +3,14 @@ def calculateTime():
     # This first line is provided for you
     monkey_one = input("Is the first monkey smiling?:  ")
     monkey_two = input("Is the second monkey smiling?: ")
+    
+    if monkey_one == 'y' and monkey_two == 'y':
+        print("Uh Oh! We're in trouble!")
+    elif monkey_one == 'n' and monkey_two == 'n':
+        print("Uh Oh! We're in trouble!")
+    else :
+        print("Yay! We're going to have a good day!")
+
 
     # end assignment
 
@@ -10,4 +18,4 @@ def calculateTime():
 ## uncomment calculateTime() and run > python monkeyCalculator.py
 ## ***IMPORTANT*** please recomment before you submit/sync your assignment.
 ## OR YOUR TEST WILL NOT RUN
-# calculateTime()
+#calculateTime()
